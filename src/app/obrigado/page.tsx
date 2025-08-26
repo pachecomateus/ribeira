9207dcb1cf9010ef19fb0d66e750e45e12b5900c
pachecomateus <mateuspachecosete@gmail.com>
2025-08-26 14:36:46 -0300

    fix: old logo and add product specs

--- src/app/obrigado/page.tsx
+++ src/app/obrigado/page.tsx
@@ -6,7 +6,7 @@ export default function Obrigado() {
     <section className="bg-[#efeeea] flex flex-col lg:flex-row items-center justify-center min-h-screen font-[family-name:var(--font-montserrat)]">
       <div className="relative lg:w-[55%] h-[100svh] flex items-center">
         <div className="block lg:hidden absolute inset-0 h-1/2 bg-gradient-to-b from-[#efeeea] via-[#efeeea]/85 to-transparent z-10"></div>
-        <h2 className="text-primary uppercase text-center text-2xl font-medium absolute z-30 w-full top-40 left-1/2 -translate-x-1/2 block lg:hidden">
+        <h2 className="text-primary uppercase text-center text-2xl font-medium absolute z-30 w-full top-32 left-1/2 -translate-x-1/2 block lg:hidden">
           Vem aí o lançamento <br /> mais aguardado de <br className="block md:hidden" /> <span className="text-secondary font-semibold">Braz Cubas</span>
         </h2>
         <Image
@@ -32,8 +32,23 @@ export default function Obrigado() {
           <p className="text-center px-12 text-primary text-xl 2xl:text-2xl">
             Obrigado! <br /> Seus dados foram enviados com sucesso, em breve entraremos em contato.
           </p>
+
+          <div className="flex flex-col items-center uppercase text-primary">
+            <div className="flex items-end text-2xl gap-3">
+              <p><span className="text-secondary text-4xl">1</span> suíte</p>
+              <div className="bg-primary w-px h-[90%]" />
+              <p><span className="text-secondary text-4xl">2</span> suítes</p>
+            </div>
+            <div className="bg-primary w-full h-px my-2" />
+            <div className="flex text-sm gap-1">
+              <p>com varanda</p>
+              |
+              <p>condomínio clube completo</p>
+            </div>
+          </div>
+
           <div className="flex items-center jutify center">
-            <Image src="/logo-ribeira.svg" alt="Ribeira" width={233} height={46} />
+            <Image src="/logo-ribeira-antigo-edit.png" alt="Ribeira" width={233} height={46} />
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <section className="bg-[#efeeea] flex flex-col lg:flex-row items-center justify-center min-h-screen font-[family-name:var(--font-montserrat)]">
       <div className="relative lg:w-[55%] h-[100svh] flex items-center">
         <div className="block lg:hidden absolute inset-0 h-1/2 bg-gradient-to-b from-[#efeeea] via-[#efeeea]/85 to-transparent z-10"></div>
-        <h2 className="text-primary uppercase text-center text-2xl font-medium absolute z-30 w-full top-40 left-1/2 -translate-x-1/2 block lg:hidden">
+        <h2 className="text-primary uppercase text-center text-2xl font-medium absolute z-30 w-full top-32 left-1/2 -translate-x-1/2 block lg:hidden">
           Vem aí o lançamento <br /> mais aguardado de <br className="block md:hidden" /> <span className="text-secondary font-semibold">Braz Cubas</span>
         </h2>
         <Image
@@ -28,13 +28,29 @@ export default function Home() {
         </Link>
       </div>
       <div className="lg:w-[45%] lg:h-[100svh] h-[70svh] md:h-[60svh] flex items-center" id="contact">
-        <div className="flex w-full h-full lg:h-4/5 flex-col items-center justify-evenly">
+        <div className="flex w-full h-full lg:h-4/5 flex-col items-center justify-evenly gap-5">
           <h2 className="text-primary uppercase text-center text-2xl font-medium hidden lg:block">
             Vem aí o lançamento <br /> mais aguardado de <br className="block md:hidden" /> <span className="text-secondary font-semibold">Braz Cubas</span>
           </h2>
+
           <Contact />
+
+          <div className="flex flex-col items-center uppercase text-primary">
+            <div className="flex items-end text-2xl gap-3">
+              <p><span className="text-secondary text-4xl">1</span> suíte</p>
+              <div className="bg-primary w-px h-[90%]" />
+              <p><span className="text-secondary text-4xl">2</span> suítes</p>
+            </div>
+            <div className="bg-primary w-full h-px my-2" />
+            <div className="flex text-sm gap-1">
+              <p>com varanda</p>
+              |
+              <p>condomínio clube completo</p>
+            </div>
+          </div>
+
           <div className="flex items-center jutify center">
-            <Image src="/logo-ribeira.svg" alt="Ribeira" width={233} height={46} />
+            <Image src="/logo-ribeira-antigo-edit.png" alt="Ribeira" width={233} height={46} />
           </div>
         </div>
       </div>
